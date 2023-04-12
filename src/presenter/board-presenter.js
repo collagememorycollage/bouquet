bfcc17e18a41f7b745cab1a78ece468721d51a91
Alex <collagememorycollage@gmail.com>
2023-04-13 01:52:28 +0300

    добавил обработчик для будущего поп апа

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -6,6 +6,7 @@ import SectionFilterReasonView from "../view/section-filter-reason-view.js";
 import SectionFilterColorView from "../view/section-filter-color-view.js";
 import SectionCatalogueView from "../view/section-catalogue-view.js";
 import CatalogCardView from "../view/catalog-card-view.js";
+import PopupView from "../view/popup-view.js";
 
 export default class BoardPresenter {
   #sectionHeroComponent = new SectionHeroView();
@@ -16,6 +17,7 @@ export default class BoardPresenter {
   #sectionCatalogueComponent = new SectionCatalogueView();
   #boardCatalogComponent = null;
   #boardBouquete = [];
+  #popupComponent = new PopupView();
 
   #header = null;
   #body = null;
@@ -38,21 +40,35 @@ export default class BoardPresenter {
     render(this.#sectionFilterColorComponent, this.#main);
     render(this.#sectionCatalogueComponent, this.#main);
 
-    this.#boardCatalogComponent =
-      this.#sectionCatalogueComponent.element.querySelector(".catalogue__list");
+    this.#boardCatalogComponent = this.#sectionCatalogueComponent.element.querySelector(".catalogue__list");
+    // this.#boardCatalogBtn = this.#sectionCatalogueComponent.element.querySelector('.catalogue__btn-wrap');
 
-    for (let i = 1; i < this.#boardBouquete.length; i++) {
-      render(
-        new CatalogCardView({
-          bouquete: this.#boardBouquete[i],
-        }),
-        this.#boardCatalogComponent
-      );
-    }
+    this.#renderBoardBouquetCard();
+    render(new PopupView({
+      bouquete: this.#boardBouquete,
+    }), this.#body);
   }
 
   init() {
     this.#boardBouquete = [...this.#bouqueteModel.bouquete];
     this.initMain();
   }
+
+  #renderBoardBouquetCard(){
+    for (let i = 1; i < this.#boardBouquete.length; i++) {
+      debugger;
+      const catalogBouqueteCard = new CatalogCardView({bouquete: this.#boardBouquete[i]});
+      catalogBouqueteCard.element.querySelector('.item-card').addEventListener('click', ()=> {
+        openPopup();
+      })
+      
+      render(catalogBouqueteCard, this.#boardCatalogComponent);
+
+    }
+    const openPopup = () =>{
+      console.log('asdas');
+    }
+  }
+
+
 }
